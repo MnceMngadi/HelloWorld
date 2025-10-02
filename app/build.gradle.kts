e66baf9af4494f82201e358cb8d355c845d89a40
HelpfullMngadi <helpfull.mngadi@addpay.africa>
2025-10-02 11:11:40 +0200

    fixed compile sdk removed some of the staff compile increased to Sdk 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.addpayafrica.helloworld"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.addpayafrica.helloworld"
