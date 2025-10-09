f7e06a53fbb10d98b8c8883d87ad7f3aef666ba3
HelpfullMngadi <helpfull.mngadi@addpay.africa>
2025-10-09 09:18:53 +0200

    Siphokazi change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.addpayafrica.helloworld"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.addpayafrica.helloworld"
